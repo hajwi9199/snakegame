a46804c45a6d86409d3990dcb04cb04b2fcc027c
Administrator <hajwi9199@naver.com>
2022-02-12 14:32:59 +0900

    upload first code

---
+++ snakegame/src/snakegame/SnakeElement.java
@@ -0,0 +1,28 @@
+package snakegame;
+
+public class SnakeElement extends BoardComponent {
+	 
+    public SnakeElement(char symbol, int xStartingLocation, int yStartingLocation) {
+        setIcon(symbol);
+        setX(xStartingLocation);
+        setY(yStartingLocation);
+    }
+     
+    public void moveLeft(Board screen, SnakeElement snake) {
+        snake.setX(getX() - 1);
+        screen.setObjectOnLocation(snake, snake.getX(), snake.getY());
+        screen.ClearScreenLocation(snake.getX() + 1, snake.getY());
+    }
+     
+    public void moveRight(Board screen, SnakeElement snake) {
+
+    }
+     
+    public void moveUp(Board screen, SnakeElement snake) {
+
+    }
+     
+    public void moveDown(Board screen, SnakeElement snake) {
+
+    }
+}
